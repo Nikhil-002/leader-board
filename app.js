b4a070b9035ee2b290ae6ac8379929ef3022be3e
Nikhil-002 <93790466+Nikhil-002@users.noreply.github.com>
2024-02-25 17:25:58 +0530

    use pool

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 import express from 'express';
-import { createConnection } from 'mysql2/promise';
+// import { createConnection } from 'mysql2/promise';
+import mysql from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 const port = 3000;
 
 // MySQL database connection configuration
-const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Nikhil@123',
-  database: 'Leaderboard',
-};
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+const connection = await pool.getConnection();
 
 // API to display current week leaderboard (Top 200)
 app.get('/leaderboard/current-week', async (req, res) => {
   try {
-    const connection = await createConnection(dbConfig);
-
-    // Your SQL query for current week leaderboard (replace with your actual query)
+    const connection = await pool.getConnection();
     const query = 'SELECT * FROM UserScores WHERE YEARWEEK(TimeStamp, 1) = YEARWEEK(CURDATE(), 1) ORDER BY Score DESC LIMIT 200';
-
     const [results] = await connection.execute(query);
-    connection.end();
-
+    connection.release();
     res.json(results);
   } catch (error) {
     console.error(error);
@@ -35,13 +40,12 @@ app.get('/leaderboard/last-week/:country', async (req, res) => {
   const { country } = req.params;
 
   try {
-    const connection = await createConnection(dbConfig);
-
+    const connection = await pool.getConnection();
     // Your SQL query for last week leaderboard by country (replace with your actual query)
-    const query = `SELECT * FROM UserScores WHERE Country = ?  AND YEARWEEK(TimeStamp, 1) = YEARWEEK(CURDATE() - INTERVAL 1 WEEK, 1)ORDER BY Score DESC LIMIT 200;`;
+    const query = `SELECT * FROM UserScores WHERE Country = ?  AND YEARWEEK(TimeStamp, 1) = YEARWEEK(CURDATE() - INTERVAL 1 WEEK, 1) ORDER BY Score DESC LIMIT 200;`;
 
     const [results] = await connection.execute(query, [country]);
-    connection.end();
+    connection.release();
 
     res.json(results);
   } catch (error) {
@@ -55,13 +59,13 @@ app.get('/user/:uid/rank', async (req, res) => {
   const { uid } = req.params;
 
   try {
-    const connection = await createConnection(dbConfig);
+    const connection = await pool.getConnection();
 
     // Your SQL query to fetch user rank by UID (replace with your actual query)
     const query = `SELECT R FROM (SELECT UID, RANK() OVER (ORDER BY Score DESC) AS R FROM UserScores) AS UserRanks WHERE UID = ?;`;
 
     const [results] = await connection.execute(query, [uid]);
-    connection.end();
+    connection.release();
 
     // res.json(results);
 
@@ -75,6 +79,7 @@ app.get('/user/:uid/rank', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 
 app.get("/", (req,res) =>{
     res.send('<h1>Hello</h1>')
